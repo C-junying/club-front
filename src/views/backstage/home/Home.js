d79c80f811682f8668783370334a73918e42ff4c
C-junying <doufukeai@163.com>
2023-05-12 15:42:28 +0800

    暂时不同home页

--- src/views/backstage/home/Home.js
+++ src/views/backstage/home/Home.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import { Button } from 'antd'
-import { http } from '@/utils/http'
-import { useNavigate } from 'react-router-dom'
+// import { Button } from 'antd'
+// import { http } from '@/utils/http'
+// import { useNavigate } from 'react-router-dom'
 
 // 后台主页
 export default function Home() {
-  const navigate = useNavigate()
-  const ajax = () => {
-    http.post('/menu/roleSelect').then((res) => {
-      console.log(res.data)
-    })
-  }
+  // const navigate = useNavigate()
+  // const ajax = () => {
+  //   http.post('/menu/roleSelect').then((res) => {
+  //     console.log(res.data)
+  //   })
+  // }
   return (
     <div>
-      <Button type="primary" onClick={ajax}>
+      {/* <Button type="primary" onClick={ajax}>
         Primary Button
       </Button>
       <Button type="primary" onClick={() => navigate('/')}>
         index
-      </Button>
+      </Button> */}
     </div>
   )
 }
